85ec0856fed379b562f3ffa67e9957cad7a876a5
Olga Smirnova <olgavsmir@gmail.com>
2015-01-25 18:28:21 +0100

    Initial commit

---
+++ app/js/tab.js
@@ -0,0 +1,17 @@
+function Tab(element) {
+
+    var navTabsElements = element.find('.nav li');
+
+    navTabsElements.on('click', function () {
+
+        $(this).addClass('active');
+        navTabsElements.not(this).removeClass('active');
+
+        var tabContentId = $(this).find('a').attr('href');
+
+        element.find('.tab').not($(tabContentId)).removeClass('active');
+        $(tabContentId).addClass('active');
+    });
+}
+
+window.Tab = Tab;
